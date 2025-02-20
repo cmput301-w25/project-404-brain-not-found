d34e859d4fcf8810bda575c474d7b051c369222a
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-02-20 13:31:50 -0700

    Separate Database Service

---
+++ code/app/src/main/java/com/example/cmput301_team_project/UserDatabaseService.java
@@ -0,0 +1,20 @@
+package com.example.cmput301_team_project;
+
+/**
+ * Singleton class to manage user-related operations with the firestore database
+ * Handles all user-related queries
+ */
+public class UserDatabaseService extends BaseDatabaseService {
+    private static UserDatabaseService instance = null;
+
+    private UserDatabaseService() {
+        super();
+    }
+
+    public UserDatabaseService getInstance() {
+        if(instance == null) {
+            instance = new UserDatabaseService();
+        }
+        return instance;
+    }
+}
