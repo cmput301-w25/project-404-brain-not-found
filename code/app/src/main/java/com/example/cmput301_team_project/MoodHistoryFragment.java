af8d9cec215bb13896c336acd9b6d7720e3791d0
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-02-20 21:19:19 -0700

    Add layout for mood history

---
+++ code/app/src/main/java/com/example/cmput301_team_project/MoodHistoryFragment.java
@@ -0,0 +1,43 @@
+package com.example.cmput301_team_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MoodHistoryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MoodHistoryFragment extends Fragment {
+
+    public MoodHistoryFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment MoodHistoryFragment.
+     */
+    public static MoodHistoryFragment newInstance() {
+        return new MoodHistoryFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_mood_history, container, false);
+    }
+}
