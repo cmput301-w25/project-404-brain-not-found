4218dc85492bc18f148c6cf098b356bc6410b60c
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-02-26 23:36:25 -0700

    Cleanup

--- code/app/src/main/java/com/example/cmput301_team_project/LoginFragment.java
+++ code/app/src/main/java/com/example/cmput301_team_project/LoginFragment.java
@@ -3,10 +3,8 @@ package com.example.cmput301_team_project;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,8 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 
 import java.security.NoSuchAlgorithmException;
