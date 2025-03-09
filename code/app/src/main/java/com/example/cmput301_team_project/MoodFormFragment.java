7e3cb34574fd041d88e7bc815e77d31e5c3dce85
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-03-09 11:26:14 -0600

    Cleanup

--- code/app/src/main/java/com/example/cmput301_team_project/MoodFormFragment.java
+++ code/app/src/main/java/com/example/cmput301_team_project/MoodFormFragment.java
@@ -5,7 +5,6 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -30,7 +29,6 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 
