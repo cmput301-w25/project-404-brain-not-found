176559f1cd96091c37ac16a11092923ac6e35f80
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-03-10 16:33:40 -0600

    UI tests fix

--- code/app/src/androidTest/java/com/example/cmput301_team_project/BaseActivityTest.java
+++ code/app/src/androidTest/java/com/example/cmput301_team_project/BaseActivityTest.java
@@ -24,53 +24,5 @@ import java.util.Objects;
 
 public class BaseActivityTest {
 
-    @BeforeClass
-    public static void setup(){
-        // Specific address for emulated device to access our localHost
-        String androidLocalhost = "10.0.2.2";
 
-        int portNumber = 8080;
-        FirebaseFirestore.getInstance().useEmulator(androidLocalhost, portNumber);
-    }
-
-    @Before
-    public void seedDatabase() {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-        CollectionReference moviesRef = db.collection("movies");
-        Mood[] moods = {
-                Mood.createMood(MoodEmotionEnum.ANGER, MoodSocialSituationEnum.ALONE, "fassdfa", "Urkel", null, null),
-                Mood.createMood(MoodEmotionEnum.SADNESS, MoodSocialSituationEnum.CROWD, "fassdfa", "Vance", null, null),
-                Mood.createMood(MoodEmotionEnum.HAPPINESS, MoodSocialSituationEnum.SEVERAL, "fassdfa", "Henrietta", null, null),
-        };
-        for (Mood mood : moods) {
-            moviesRef.document().set(mood);
-        }
-    }
-
-
-
-    @After
-    public void tearDown() {
-        String projectId = "cmput301-project-d122a";
-        URL url = null;
-        try {
-            url = new URL("http://10.0.2.2:8080/emulator/v1/projects/" + projectId + "/databases/(default)/documents");
-        } catch (MalformedURLException exception) {
-            Log.e("URL Error", Objects.requireNonNull(exception.getMessage()));
-        }
-        HttpURLConnection urlConnection = null;
-        try {
-            urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setRequestMethod("DELETE");
-            int response = urlConnection.getResponseCode();
-            Log.i("Response Code", "Response Code: " + response);
-        } catch (IOException exception) {
-            Log.e("IO Error", Objects.requireNonNull(exception.getMessage()));
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
-
-    }
 }
