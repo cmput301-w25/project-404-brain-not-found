a39de524b6e487e534db1a1f83e53cf5ea31c8ca
Kelsey-Patti <kelseypattison1@gmail.com>
2025-03-19 02:56:13 -0600

    added profile page for other users

--- code/app/src/main/java/com/example/cmput301_team_project/ui/BaseUserListFragment.java
+++ code/app/src/main/java/com/example/cmput301_team_project/ui/BaseUserListFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SearchView;
 
@@ -74,7 +75,14 @@ public abstract class BaseUserListFragment extends Fragment {
                 return true;
             }
         });
-
+        userList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                PublicUser user = userAdapter.getItem(position);
+                ViewProfileFragment.newInstance(user.getUsername(), user.getName()).show(requireActivity().getSupportFragmentManager(), "Profile");
+            }
+        });
         return view;
     }
+
 }
