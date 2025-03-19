bfcc69b9d3efb51e1b4b8cf6ad53a57235aaec2c
Kelsey-Patti <kelseypattison1@gmail.com>
2025-03-19 16:06:47 -0600

    removed import

--- code/app/src/main/java/com/example/cmput301_team_project/ui/MainActivity.java
+++ code/app/src/main/java/com/example/cmput301_team_project/ui/MainActivity.java
@@ -13,7 +13,6 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.cmput301_team_project.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import org.w3c.dom.Text;
 
 /**
  * The main activity that serves as the entry point for the application after login.
