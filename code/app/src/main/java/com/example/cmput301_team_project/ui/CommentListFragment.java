83f53e48ee2be5e6f3443c8bd0e89d7e1ea5fb32
Kelsey-Patti <kelseypattison1@gmail.com>
2025-03-21 12:52:21 -0600

    added Comment list fragment

---
+++ code/app/src/main/java/com/example/cmput301_team_project/ui/CommentListFragment.java
@@ -0,0 +1,9 @@
+package com.example.cmput301_team_project.ui;
+
+import android.app.Dialog;
+
+import androidx.fragment.app.DialogFragment;
+
+public class CommentListFragment extends DialogFragment {
+
+}
