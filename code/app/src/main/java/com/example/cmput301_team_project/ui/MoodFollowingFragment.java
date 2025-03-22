610d806d91ea4760d39402eff7ec5e14348e55a3
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-03-22 17:42:29 -0600

    Better generalize mood lists

--- code/app/src/main/java/com/example/cmput301_team_project/ui/MoodFollowingFragment.java
+++ code/app/src/main/java/com/example/cmput301_team_project/ui/MoodFollowingFragment.java
@@ -1,13 +1,6 @@
 package com.example.cmput301_team_project.ui;
 
-import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.example.cmput301_team_project.R;
 import com.example.cmput301_team_project.SessionManager;
@@ -24,7 +17,6 @@ import java.util.ArrayList;
 public class MoodFollowingFragment extends BaseMoodListFragment {
     private final MoodDatabaseService moodDatabaseService;
     private final UserDatabaseService userDatabaseService;
-    private MoodListAdapter moodListAdapter;
 
     public MoodFollowingFragment() {
         moodDatabaseService = MoodDatabaseService.getInstance();
@@ -42,17 +34,6 @@ public class MoodFollowingFragment extends BaseMoodListFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_mood_following, container, false);
-        ListView moodListView = view.findViewById(R.id.mood_List);
-        moodListAdapter = new MoodListAdapter(getContext(), new ArrayList<>(), this, false);
-        moodListView.setAdapter(moodListAdapter);
-        // Inflate the layout for this fragment
-        return view;
-    }
-
-    @Override
     protected void loadMoodData() {
         userDatabaseService.getFollowing(SessionManager.getInstance().getCurrentUser())
                         .addOnSuccessListener(following -> moodDatabaseService.getFollowingMoods(following)
@@ -61,5 +42,15 @@ public class MoodFollowingFragment extends BaseMoodListFragment {
                                     moodListAdapter.addAll(moods);
                                     moodListAdapter.notifyDataSetChanged();
                                 }));
+    }
+
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.fragment_mood_following;
+    }
+
+    @Override
+    protected boolean isMoodOwned() {
+        return false;
     }
 }
--- code/app/src/main/java/com/example/cmput301_team_project/ui/MoodHistoryFragment.java
+++ code/app/src/main/java/com/example/cmput301_team_project/ui/MoodHistoryFragment.java
@@ -4,23 +4,14 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.cmput301_team_project.R;
 import com.example.cmput301_team_project.SessionManager;
 import com.example.cmput301_team_project.db.MoodDatabaseService;
-import com.example.cmput301_team_project.model.Comment;
 import com.example.cmput301_team_project.model.Mood;
-import com.google.android.gms.tasks.Task;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass for user mood history screen.
@@ -30,14 +21,11 @@ import java.util.List;
  */
 public class MoodHistoryFragment extends BaseMoodListFragment implements MoodFormFragment.MoodFormDialogListener {
     private final MoodDatabaseService moodDatabaseService;
-    private MoodListAdapter moodListAdapter;
-    private ArrayList<Mood> moodList;
 
 
 
     public MoodHistoryFragment() {
         moodDatabaseService = MoodDatabaseService.getInstance();
-        moodList = new ArrayList<>();
 
     }
 
@@ -57,29 +45,11 @@ public class MoodHistoryFragment extends BaseMoodListFragment implements MoodFor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_mood_history, container, false);
-        ListView moodListView = view.findViewById(R.id.mood_List);
-        moodListAdapter = new MoodListAdapter(getContext(), moodList, this, true);
-        moodListView.setAdapter(moodListAdapter);
-
-
-
-
+    protected void setupUI(View view) {
         ImageButton addMoodButton = view.findViewById(R.id.add_mood_button);
         addMoodButton.setOnClickListener(v -> {
             MoodFormFragment.newInstance(null).show(getChildFragmentManager(), "Add Mood Event");
         });
-
-        moodListView.setOnItemClickListener((parent, view1, position, id) -> {
-            Mood selectedMood = (Mood) parent.getItemAtPosition(position);
-            String moodId = selectedMood.getId();
-            CommentListFragment.newInstance(moodId).show(requireActivity().getSupportFragmentManager(), "CommentListFragment");
-
-        });
-        loadMoodData();
-        return view;
     }
 
     @Override
@@ -97,12 +67,13 @@ public class MoodHistoryFragment extends BaseMoodListFragment implements MoodFor
     /**
      * Loads mood data from Firestore database
      */
+    @Override
     protected void loadMoodData() {
         moodDatabaseService.getMoodList(SessionManager.getInstance().getCurrentUser())
                 .addOnSuccessListener(moods -> {
                     // Clear existing list and add new data
-                    moodList.clear();
-                    moodList.addAll(moods);
+                    moodListAdapter.clear();
+                    moodListAdapter.addAll(moods);
 
                     // Notify adapter that data has changed
                     moodListAdapter.notifyDataSetChanged();
@@ -114,5 +85,14 @@ public class MoodHistoryFragment extends BaseMoodListFragment implements MoodFor
                 });
     }
 
+    @Override
+    protected int getLayoutRes() {
+        return R.layout.fragment_mood_history;
+    }
+
+    @Override
+    protected boolean isMoodOwned() {
+        return true;
+    }
 
 }
