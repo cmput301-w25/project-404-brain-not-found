8ab1d799323c5ae37b94e4de9d189ac6c07f9158
harneet862 <144798379+harneet862@users.noreply.github.com>
2025-03-24 19:57:08 -0600

    UI changed

--- code/app/src/main/java/com/example/cmput301_team_project/ui/ViewProfileFragment.java
+++ code/app/src/main/java/com/example/cmput301_team_project/ui/ViewProfileFragment.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,9 +23,9 @@ import com.example.cmput301_team_project.model.Mood;
 public class ViewProfileFragment extends DialogFragment {
 
 
-    public static ViewProfileFragment newInstance(String username, String name){
+    public static ViewProfileFragment newInstance(String username, String name) {
         Bundle args = new Bundle();
-        args.putSerializable("username",username);
+        args.putSerializable("username", username);
         args.putSerializable("name", name);
         ViewProfileFragment fragment = new ViewProfileFragment();
         fragment.setArguments(args);
@@ -33,14 +34,15 @@ public class ViewProfileFragment extends DialogFragment {
 
     @NonNull
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState){
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
         UserDatabaseService udb = UserDatabaseService.getInstance();
         View view = LayoutInflater.from(getContext()).inflate(R.layout.fragment_profile, null);
         String name = (String) getArguments().getSerializable("name");
         String username = (String) getArguments().getSerializable("username");
         TextView publicName = view.findViewById(R.id.publicName);
         TextView publicUsername = view.findViewById(R.id.publicUsername);
-        publicUsername.setText("@"+username);
+        publicUsername.setText("@" + username);
+        publicName.setText(name);
         publicName.setText(name);
 
         TextView followerCount = view.findViewById(R.id.followerCount);
@@ -53,19 +55,32 @@ public class ViewProfileFragment extends DialogFragment {
         });
         TextView emoji = view.findViewById(R.id.recentEmoji);
         MoodDatabaseService mdb = MoodDatabaseService.getInstance();
-        mdb.getMostRecentMood(username).addOnSuccessListener(emotion->{
-            if (emotion != null){
+        mdb.getMostRecentMood(username).addOnSuccessListener(emotion -> {
+            if (emotion != null) {
                 Mood tempMood = Mood.createMood(MoodEmotionEnum.valueOf(emotion), null, null, false, null, null, null, null);
                 emoji.setText(tempMood.getEmoji());
             }
         });
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        return builder
+        AlertDialog dialog = new AlertDialog.Builder(getContext())
                 .setView(view)
                 .setNegativeButton("Close", null)
                 .create();
 
+        // Set background and button color safely after the dialog is shown
+        dialog.setOnShowListener(d -> {
+            if (dialog.getWindow() != null) {
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.background)));
+            }
 
+            // Get the button after the dialog is shown
+            Button negativeButton = dialog.getButton(AlertDialog.BUTTON_NEGATIVE);
+            if (negativeButton != null) {
+                negativeButton.setTextColor(ContextCompat.getColor(requireContext(), R.color.black));
+            }
+        });
+
+        return dialog;
     }
+
 }
