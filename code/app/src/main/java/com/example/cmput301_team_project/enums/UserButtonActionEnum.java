c4b20fe8221c755cbc33ac42774923d9c09499b3
Brayden Bartelds <149531080+braydenmcb@users.noreply.github.com>
2025-03-27 11:27:32 -0600

    added documentation to all enum classes

--- code/app/src/main/java/com/example/cmput301_team_project/enums/UserButtonActionEnum.java
+++ code/app/src/main/java/com/example/cmput301_team_project/enums/UserButtonActionEnum.java
@@ -1,5 +1,12 @@
 package com.example.cmput301_team_project.enums;
 
+/**
+ * Enumeration representing the actions the user can perform on another user.
+ *
+ * FOLLOW - request to follow the other user.
+ * UNFOLLOW - unfollow the other user.
+ * REMOVE - remove the other user from the current user's followers list.
+ */
 public enum UserButtonActionEnum {
     FOLLOW,
     UNFOLLOW,
