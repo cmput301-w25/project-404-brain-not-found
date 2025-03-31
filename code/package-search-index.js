26931ed359a0d2c41512b7a147d8d7a1e579fb4b
Brayden Bartelds <yittrixjr@gmail.com>
2025-03-31 16:55:29 -0600

    fixed javadoc

--- code/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.cmput301_team_project"},{"l":"com.example.cmput301_team_project.db"},{"l":"com.example.cmput301_team_project.enums"},{"l":"com.example.cmput301_team_project.model"},{"l":"com.example.cmput301_team_project.ui"},{"l":"com.example.cmput301_team_project.utils"}];updateSearchResults();
