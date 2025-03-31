d9bd370cfc571c0906110577be33a5ece26c45df
daniil-zahoruiko <daniel.zagoruyko@gmail.com>
2025-03-31 00:48:08 -0600

    Add more user UI tests

--- code/app/src/androidTest/java/com/example/cmput301_team_project/UserInstrumentedTest.java
+++ code/app/src/androidTest/java/com/example/cmput301_team_project/UserInstrumentedTest.java
@@ -2,13 +2,19 @@ package com.example.cmput301_team_project;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressKey;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.containsString;
+
+import android.view.KeyEvent;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -58,7 +64,8 @@ public class UserInstrumentedTest extends BaseActivityTest {
         AppUser[] users = {
                 new AppUser("Henrietta", "Test Display", "some_password"),
                 new AppUser("Vance", "Vancey", "vance123"),
-                new AppUser("Urkel", "Lekru", "urkel321")
+                new AppUser("Urkel", "Lekru", "urkel321"),
+                new AppUser("thor123", "Chris Hemsworth", "qwerty")
         };
 
         for (AppUser user: users) {
@@ -76,6 +83,8 @@ public class UserInstrumentedTest extends BaseActivityTest {
         userDatabaseService = UserDatabaseService.getInstance();
 
         userDatabaseService.requestFollow("Vance", "Henrietta");
+        userDatabaseService.requestFollow("Henrietta", "Urkel");
+        userDatabaseService.acceptRequest("Henrietta", "Urkel");
 
         Thread.sleep(3000);
         FirebaseAuthenticationService.getInstance().setUserForTesting("Henrietta");
@@ -121,6 +130,22 @@ public class UserInstrumentedTest extends BaseActivityTest {
     }
 
     @Test
+    public void testRemoveFollower() throws InterruptedException {
+        onView(withId(R.id.user_icon)).perform(click());
+        Thread.sleep(2000);
+
+        onView(withText("Accept")).perform(click());
+        Thread.sleep(500);
+        onView(withText("Vance")).check(doesNotExist());
+        onView(withText("Followers")).perform(click());
+        Thread.sleep(1000);
+
+        onView(withText("Remove")).perform(click());
+        Thread.sleep(500);
+        onView(withText("Vance")).check(doesNotExist());
+    }
+
+    @Test
     public void testDeclineRequest() throws InterruptedException {
         onView(withId(R.id.user_icon)).perform(click());
         Thread.sleep(2000);
@@ -132,5 +157,74 @@ public class UserInstrumentedTest extends BaseActivityTest {
         Thread.sleep(1000);
 
         onView(withText("Vance")).check(doesNotExist());
+    }
+
+    @Test
+    public void testFollowing() throws InterruptedException {
+        onView(withId(R.id.user_icon)).perform(click());
+        Thread.sleep(2000);
+
+        onView(withText("Following")).perform(click());
+        Thread.sleep(1000);
+        onView(allOf(getElementFromMatchAtPosition(allOf(withText("Urkel")), 1))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testUnfollow() throws InterruptedException {
+        onView(withId(R.id.user_icon)).perform(click());
+        Thread.sleep(2000);
+
+        onView(withText("Following")).perform(click());
+        Thread.sleep(1000);
+        onView(withText("Unfollow")).perform(click());
+        Thread.sleep(500);
+        onView(allOf(getElementFromMatchAtPosition(allOf(withText("Urkel")), 1))).check(doesNotExist());
+    }
+
+    @Test
+    public void testSearch() throws InterruptedException {
+        onView(withId(R.id.user_icon)).perform(click());
+        Thread.sleep(2000);
+
+        onView(withText("People")).perform(click());
+        Thread.sleep(1000);
+        onView(allOf(getElementFromMatchAtPosition(allOf(withId(R.id.user_search)), 0))).perform(typeText("ThOr"));
+        onView(allOf(getElementFromMatchAtPosition(allOf(withId(R.id.user_search)), 0))).perform(closeSoftKeyboard());
+        onView(allOf(getElementFromMatchAtPosition(allOf(withId(R.id.user_search)), 0))).perform(pressKey(KeyEvent.KEYCODE_ENTER));
+        Thread.sleep(500);
+        onView(withText("thor123")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testOthersProfile() throws InterruptedException {
+        onView(withId(R.id.user_icon)).perform(click());
+        Thread.sleep(2000);
+
+        onView(withText("People")).perform(click());
+        Thread.sleep(1000);
+        onView(allOf(getElementFromMatchAtPosition(allOf(withId(R.id.user_search)), 0))).perform(typeText("ThOr"));
+        onView(allOf(getElementFromMatchAtPosition(allOf(withId(R.id.user_search)), 0))).perform(closeSoftKeyboard());
+        onView(allOf(getElementFromMatchAtPosition(allOf(withId(R.id.user_search)), 0))).perform(pressKey(KeyEvent.KEYCODE_ENTER));
+        Thread.sleep(500);
+        onView(withText("thor123")).perform(click());
+        Thread.sleep(500);
+        onView(withId(R.id.publicUsername)).check(matches(withText("@thor123")));
+        onView(withId(R.id.publicName)).check(matches(withText("Chris Hemsworth")));
+    }
+
+    @Test
+    public void testFollow() throws InterruptedException {
+        onView(withId(R.id.user_icon)).perform(click());
+        Thread.sleep(2000);
+
+        onView(withText("People")).perform(click());
+        Thread.sleep(1000);
+        onView(allOf(getElementFromMatchAtPosition(allOf(withId(R.id.user_search)), 0))).perform(typeText("ThOr"));
+        onView(allOf(getElementFromMatchAtPosition(allOf(withId(R.id.user_search)), 0))).perform(closeSoftKeyboard());
+        onView(allOf(getElementFromMatchAtPosition(allOf(withId(R.id.user_search)), 0))).perform(pressKey(KeyEvent.KEYCODE_ENTER));
+        Thread.sleep(500);
+        onView(withText("Follow")).perform(click());
+        Thread.sleep(500);
+        onView(withText("Requested")).check(matches(isDisplayed()));
     }
 }
